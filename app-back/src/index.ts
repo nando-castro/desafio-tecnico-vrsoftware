5f42a3a61bd1c49ef5828d0c8687009913eb1092
Fernando <fernando.castro10.master@gmail.com>
2023-02-03 18:01:12 -0300

    fix: name error middleware

--- app-back/src/index.ts
+++ app-back/src/index.ts
@@ -2,10 +2,12 @@ import express, { json } from "express";
 import cors from "cors";
 import "express-async-errors";
 import router from "./routes";
+import handleErrorMiddleware from "./middlewares/handleErrorMiddleware";
 
 const app = express();
 app.use(json());
 app.use(cors());
 app.use(router);
+app.use(handleErrorMiddleware)
 
 export default app;
