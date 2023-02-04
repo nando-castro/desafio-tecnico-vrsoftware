5404cf6f077069e93bfce510b0456b9232b438bc
Fernando <fernando.castro10.master@gmail.com>
2023-02-04 18:03:30 -0300

    feat: config initial for front-end

---
+++ app-front-react/src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ResetCSS from "./assets/css/reset";
+import GlobalStyle from "./assets/css/global";
+import { Home } from "./pages/home/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ResetCSS />
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
