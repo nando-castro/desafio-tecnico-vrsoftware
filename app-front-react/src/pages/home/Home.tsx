8fa0da95d90367784c3ed3d7854c767c1d23b2a3
Fernando <fernando.castro10.master@gmail.com>
2023-02-04 19:26:48 -0300

    feat: create component button

--- app-front-react/src/pages/home/Home.tsx
+++ app-front-react/src/pages/home/Home.tsx
@@ -1,3 +1,16 @@
+import styled from "styled-components";
+
 export const Home = () => {
-  return <>Home Page</>;
+  return (
+    <Container>
+      Home
+    </Container>
+  );
 };
+
+const Container = styled.main`
+  width: 100%;
+  height: 100vh;
+  color: #FFFFFF;
+  background-color: #124090;
+`;
