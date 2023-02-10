a07d390bf4453b3e57f4061db1344e335ce55b82
Fernando <fernando.castro10.master@gmail.com>
2023-02-10 10:26:36 -0300

    feat: created components footer and header

--- app-front-angular/src/app/app.module.ts
+++ app-front-angular/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
